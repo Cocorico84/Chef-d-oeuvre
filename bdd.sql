4590e52de267f8b964f67b12066b250a4cef20df
Cocorico84 <corentin.zheng@gmail.com>
2018-07-30 15:36:23 +0200

    modif

--- bdd.sql
+++ bdd.sql
@@ -5,11 +5,13 @@ CREATE TABLE habits
 	id_habits INT PRIMARY KEY,
 	type VARCHAR(50),
 	sexe VARCHAR(2),
-	prix INT
+	marque VARCHAR(50),
+	prix INT,
+	lien VARCHAR(150)
 )
 
 CREATE TABLE pays;
 (
 	id_pays INT PRIMARY KEY,
-
+	nom VARCHAR(50)
 )
