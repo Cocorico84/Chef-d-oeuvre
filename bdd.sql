070d3b72b3c796c1ce0b38d238db954d3f271eb7
Cocorico84 <corentin.zheng@gmail.com>
2018-07-30 15:19:35 +0200

    bdd

---
+++ bdd.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE oeuvre;
+
+CREATE TABLE habits
+(
+	id_habits INT PRIMARY KEY,
+	type VARCHAR(50),
+	sexe VARCHAR(2),
+	prix INT
+)
+
+CREATE TABLE pays;
+(
+	id_pays INT PRIMARY KEY,
+
+)
